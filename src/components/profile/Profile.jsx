cba49d42cdcee4696ac3093d5c6e0f01c4a13806
davidsilva131 <davidsilvac131@gmail.com>
2022-11-09 19:21:11 -0500

    Add profile page design

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,7 +1,95 @@
+import { Avatar, CardActionArea } from "@mui/material";
+import { Container } from "@mui/system";
 import React from "react";
-
+import ArrowForward from '@mui/icons-material/ArrowForwardIos';
+import PersonIcon from '@mui/icons-material/Person';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import Switch from '@mui/material/Switch';
+import PaymentIcon from '@mui/icons-material/Payment';
+import LanguageIcon from '@mui/icons-material/Language';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import PhoneIcon from '@mui/icons-material/Phone';
+import './Profile.scss'
 const Profile = () => {
-    return <div>Profile</div>;
+    return (
+        <Container>
+            <div className="profile">
+                <aside className="profile__image">
+                    <div>
+                        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg"
+                            sx={{ width: '64px', height: '64px' }} />
+                        <span className="profile__name">Juan David Silva Contreras</span>
+                    </div>
+                </aside>
+                <aside className="profile__options">
+                    <CardActionArea sx={{ width: '100%', heigth: '50px', borderRadius: '10px' }}>
+                        <div className="profile__options__card">
+                            <div>
+                                <PersonIcon />
+                                <span>Acount Edit</span>
+                            </div>
+                            <ArrowForward />
+                        </div>
+                    </CardActionArea>
+                    <CardActionArea sx={{ width: '100%', heigth: '50px', borderRadius: '10px' }}>
+                        <div className="profile__options__card">
+                            <div>
+                                <NotificationsIcon />
+                                <span>Notifications</span>
+                            </div>
+                            <Switch defaultChecked />
+                        </div>
+                    </CardActionArea>
+                    <CardActionArea sx={{ width: '100%', heigth: '50px', borderRadius: '10px' }}>
+                        <div className="profile__options__card">
+                            <div>
+                                <PaymentIcon />
+                                <span>Payment</span>
+                            </div>
+                            <ArrowForward />
+                        </div>
+                    </CardActionArea>
+                    <CardActionArea sx={{ width: '100%', heigth: '50px', borderRadius: '10px' }}>
+                        <div className="profile__options__card">
+                            <div>
+                                <LanguageIcon />
+                                <span>Language</span>
+                            </div>
+                            <ArrowForward />
+                        </div>
+                    </CardActionArea>
+                    <CardActionArea sx={{ width: '100%', heigth: '50px', borderRadius: '10px' }}>
+                        <div className="profile__options__card">
+                            <div>
+                                <LocationOnIcon />
+                                <span>Location</span>
+                            </div>
+                            <ArrowForward />
+                        </div>
+                    </CardActionArea>
+                    <CardActionArea sx={{ width: '100%', heigth: '50px', borderRadius: '10px' }}>
+                        <div className="profile__options__card">
+                            <div>
+                                <HelpOutlineIcon />
+                                <span>FAQ</span>
+                            </div>
+                            <ArrowForward />
+                        </div>
+                    </CardActionArea>
+                    <CardActionArea sx={{ width: '100%', heigth: '50px', borderRadius: '10px' }}>
+                        <div className="profile__options__card">
+                            <div>
+                                <PhoneIcon />
+                                <span>Support</span>
+                            </div>
+                            <ArrowForward />
+                        </div>
+                    </CardActionArea>
+                </aside>
+            </div>
+        </Container>
+    )
 };
 
 export default Profile;
